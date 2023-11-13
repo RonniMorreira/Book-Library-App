50a9a5bc31043103dd1dd22d7fb43a61e60f8a20
Aslan Khetagurov <lampi15rus@gmail.com>
2023-11-13 13:59:31 +0300

    Added book id

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 
@@ -14,11 +15,11 @@ const BookForm = () => {
       const book = {
         title,
         author,
+        id: uuidv4(),
       };
 
       dispatch(addBook(book));
 
-      // console.log(title, author);
       setTitle('');
       setAuthor('');
     }
