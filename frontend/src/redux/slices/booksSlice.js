ef9afae5fc0ea90285a50ffafa91b0acfd3b0821
Aslan Khetagurov <lampi15rus@gmail.com>
2023-11-19 15:47:12 +0300

    Added createAsyncThunk

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,8 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import createBookWithID from '../../utils/createBookWithID';
 
 const initialState = [];
+
+export const fetchBook = createAsyncThunk('books/fetchBook', async () => {
+  const res = await axios.get('http://localhost:5000/random-book');
+  return res.data;
+});
 
 const booksSlice = createSlice({
   name: 'books',
@@ -34,22 +39,17 @@ const booksSlice = createSlice({
       //   );
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBook.fulfilled, (state, action) => {
+      if (action.payload.title && action.payload.author) {
+        state.push(createBookWithID(action.payload, 'API'));
+      }
+    });
+  },
 });
 
 export const { setAddBook, setDeleteBook, setToggleFavorite } =
   booksSlice.actions;
-
-export const thunkFunction = async (dispatch, getState) => {
-  try {
-    const res = await axios.get('http://localhost:4000/random-book');
-    // используем оператор опциональной цепочки вместо условия "if (res.data && res.data.title && res.data.author)"
-    if (res?.data?.title && res?.data?.author) {
-      dispatch(setAddBook(createBookWithID(res.data, 'API')));
-    }
-  } catch (error) {
-    console.log('Error fetching random book', error);
-  }
-};
 
 export const selectBooks = (state) => state.books;
 
